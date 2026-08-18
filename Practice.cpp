b2b4e455e04f8c2d066c0da2586973c625ad8498
afaiyaz-dev <abdullahfaiyaz462@gmail.com>
2026-08-17 23:57:34 -0700

    Initial commit

---
+++ Practice.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct student{
+    string name;
+    int rollno;
+    vector <float> marks;
+    char grade;
+};
+
+int main(){
+    int X;
+    vector <student> st;
+
+    while(true){
+        cout<<"Add a new student";
+        cout<<"Display all students";
+        cout<<"Save records to file";
+        cout<<"Exit";
+    }
+}
