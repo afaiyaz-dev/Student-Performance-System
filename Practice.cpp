0726ec48d0afbe89398d8eab54a36b74cebd26da
afaiyaz-dev <abdullahfaiyaz462@gmail.com>
2026-08-24 19:39:26 -0700

    Added a new menu including a while loop

--- Practice.cpp
+++ Practice.cpp
@@ -10,8 +10,24 @@ struct student{
 };
 
 int main(){
-    int X;
-    vector <student> st;
+    int input;
+    cout<<"Do you want to go to the menu "<<endl;
+    cout<<"press 'Y' for yes or 'N' for no "<<endl;
+    cin>>input;
 
-    cout<<"Enter student name";
+    while( input == 'Y' || input == 'y'){
+        cout<<"\t\tMenu";
+        cout<<"1. Add New Student"<<endl;
+        cout<<"2. Display All Students"<<endl;
+        cout<<"3. Search Student by Roll Number"<<endl;
+        cout<<"4. Save Records to File"<<endl;
+        cout<<"5. Exit"<<endl;
+        int choice;
+        cout<<"Enter the number to perform the following operations";
+        cin>>choice;
+        switch(choice){
+            
+        }
+
+    }
 }
